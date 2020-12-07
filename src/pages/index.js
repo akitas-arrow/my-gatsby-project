e081dbf9dd2fc1ebde3245b52487f1012c794ed8
Ayano Taki <ayanobayern2@gmail.com>
2020-12-07 21:31:38 +0900

    ファイル作成

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import React from "react"
+import Layout from "../components/Layout"
 
 
 const IndexPage = () => {
   return (
-    <></>
+    <Layout>
+      Home
+    </Layout>
   )
 }
 
---
+++ src/pages/lineUp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+function lineUp() {
+    return (
+        <Layout>
+            Line Up
+        </Layout>
+    )
+}
+
+export default lineUp
