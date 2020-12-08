855470c8ed8cd36c84d3b908d86e7fda28539256
Ayano Taki <ayanobayern2@gmail.com>
2020-12-08 23:29:16 +0900

    GlobalCSS作成

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Header() {
+    return (
+        <Wrapper>
+            Header
+        </Wrapper>
+    )
+}
+
+const Wrapper = styled.header`
+ background-color:pink;
+`
+
+export default Header
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,28 @@
 import React from 'react'
-import Header from './Header'
-import Footer from './Footer'
+import { createGlobalStyle } from 'styled-components'
+import "modern-css-reset"
+import "fontsource-noto-sans-jp"
+import Header from './Header/Header'
+import Footer from './Footer/Footer'
 
 function Layout({ children }) {
     return (
         <div>
+            <GlobalStyle />
             <Header />
                 { children }
             <Footer />
         </div>
     )
 }
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        font-family: "Noto Sans JP";
+        line-height: 2em;
+        letter-spacing: 0.05em;
+        color: #333333;
+    }
+`
 
 export default Layout
