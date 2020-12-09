83cba2cc5f50d5eead53a34b3697133584eb39d1
Ayano Taki <ayanobayern2@gmail.com>
2020-12-09 22:55:25 +0900

    フッタースタイル

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
 import styled from 'styled-components'
 import { Color } from '../shared/style.js'
 
 function Footer() {
+    const data = useStaticQuery(graphql`
+        query {
+            file(relativePath: {eq: "logo01.svg"}){
+                publicURL
+            }
+        }
+    `)
     return (
         <Wrapper>
             <Container>
-                Footer
+                <ImageStyle src={data.file.publicURL} alt='有限会社トモエ屋' />
             </Container>
         </Wrapper>
     )
@@ -20,6 +28,10 @@ const Container = styled.div`
     border-top: 3px dotted ${Color.peacock};
     max-width: 1100px;
     margin: 0 auto;
+`
+
+const ImageStyle = styled.img`
+    height:56px;
 `
 
 
