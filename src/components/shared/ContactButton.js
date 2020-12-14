c1a911a0877deeae240847154cc5fb7f8d297030
Ayano Taki <ayanobayern2@gmail.com>
2020-12-14 22:06:41 +0900

    contactButton

---
+++ src/components/shared/ContactButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ContactButton() {
+    return (
+        <div>
+            <p>contact</p>
+            {/* <FontAwesomeIcon icon={['far', 'youtube']} color={Color.letter}/> */}
+        </div>
+    )
+}
+
+export default ContactButton
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Layout from "../components/Layout"
+import ContactButton from "../components/shared/ContactButton"
 
 
 const IndexPage = () => {
   return (
     <Layout>
       Home
+      <ContactButton />
     </Layout>
   )
 }
