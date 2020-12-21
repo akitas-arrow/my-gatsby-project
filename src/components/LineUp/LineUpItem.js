819b20a7d1e7d721c86e0fa9b8e57817baaa089a
Ayano Taki <ayanobayern2@gmail.com>
2020-12-21 23:59:35 +0900

    取扱商品ページスタイル

--- src/components/LineUp/LineUpItem.js
+++ src/components/LineUp/LineUpItem.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import Wrapper from '../shared/Wrapper'
 import Container from '../shared/Container'
+import {MediumTextStyle, BoldTextStyle} from '../shared/style'
 
 function LineUpItem({title, description, items}) {
     return (
@@ -15,7 +16,7 @@ function LineUpItem({title, description, items}) {
                     <p>
                         {description}
                     </p>
-                    <p>取扱商品</p>
+                    <ItemHeader>取扱商品</ItemHeader>
                     <p>{items}</p>
                 </TextBlock>
             </Container>
@@ -44,7 +45,20 @@ const TextBlock = styled.div`
 `
 
 const Title = styled.h4`
+    /* padding-top: 72px; */
+    padding-bottom: 48px;
+    /* text-align: center; */
+    ${BoldTextStyle}
+    font-size: 18px;
+    @media (min-width: 768px) {
+        /* padding-top: 112px; */
+        padding-bottom: 64px;
+        font-size: 26px;
+    }
+`
 
+const ItemHeader = styled.p`
+    ${MediumTextStyle}
 `
 
 export default LineUpItem
