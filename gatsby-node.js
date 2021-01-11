fb5952e278d2fa4a83ae4e41cb078a634b151483
Ayano Taki <ayanobayern2@gmail.com>
2021-01-11 23:58:54 +0900

    pagenationきのう

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,10 @@
 const path = require('path');
+const { paginate } = require("gatsby-awesome-pagination");
+
 exports.createPages = async ({ graphql, actions }) => {
     const { createPage } = actions;
     const blogTemplate = path.resolve('./src/Templates/BlogDetail.js')
+    const blogsTemplate = path.resolve('./src/Templates/Blogs.js')
 
     const result = await graphql(`
         {
