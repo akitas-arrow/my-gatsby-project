e1d4f30180f9e6812ded72561c735e953573fad0
Ayano Taki <ayanobayern2@gmail.com>
2021-02-04 16:01:28 +0900

    メインイメージ変更

--- src/components/Home/TopImage.js
+++ src/components/Home/TopImage.js
@@ -8,9 +8,9 @@ function TopImage() {
 
   const data = useStaticQuery(graphql`
     query {
-      TopImage01:file(relativePath:{eq:"topimg06.png"}) {
+      TopImage01:file(relativePath:{eq:"topimg001.png"}) {
         childImageSharp {
-          fluid(maxWidth:2000) {
+          fluid(maxWidth:1920) {
             ...GatsbyImageSharpFluid
           }
         }
@@ -26,44 +26,45 @@ function TopImage() {
   `)
 
   return (
-    <Box>
-        <Title>日用品を通し清潔で健康な<br/>暮らしに貢献します</Title>
+    // <Box>
+    //     {/* <Title>日用品を通し清潔で健康な<br/>暮らしに貢献します</Title> */}
+    //     <Image fluid={data.TopImage01.childImageSharp.fluid}/>
+    // </Box>
         <Image fluid={data.TopImage01.childImageSharp.fluid}/>
-    </Box>
   )
 }
 
-const Box = styled.div`
-  position: relative;
-  height: 80vh;
-  width: 100%;
-  @media (min-width: 768px) {
-    height: 100vh;
-  }
-`
+// const Box = styled.div`
+//   position: relative;
+//   height: 80vh;
+//   width: 100%;
+//   @media (min-width: 768px) {
+//     height: 100vh;
+//   }
+// `
 
-const Title = styled.h1`
-  ${BoldTextStyle}
-  color: ${Color.main};
-  position: absolute;
-  width: 80%;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  z-index: 1;
-  font-size: 22px;
-  @media (min-width: 768px) {
-    width: auto;
-    font-size: 40px;
-  }
-`
+// const Title = styled.h1`
+//   ${BoldTextStyle}
+//   color: ${Color.main};
+//   position: absolute;
+//   width: 80%;
+//   top: 50%;
+//   left: 50%;
+//   transform: translate(-50%, -50%);
+//   z-index: 1;
+//   font-size: 22px;
+//   @media (min-width: 768px) {
+//     width: auto;
+//     font-size: 40px;
+//   }
+// `
 
 const Image = styled(Img)`
   width: 100%;
   /* height: 80vh; */
-  height: 100%;
-  object-fit: cover;
-  object-position: center bottom;
+  height: 100vh;
+  /* object-fit: cover;
+  object-position: center bottom; */
   /* @media (min-width:768px) {
     height: 100vh;
   } */
