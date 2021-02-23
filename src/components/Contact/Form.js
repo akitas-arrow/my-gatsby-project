87ef5714f3f2acd2410b8770ae5fb9e48fef90ee
Ayano Taki <ayanobayern2@gmail.com>
2021-02-23 19:31:04 +0900

    バリデーションチェック実装

--- src/components/Contact/Form.js
+++ src/components/Contact/Form.js
@@ -4,8 +4,26 @@ import Wrapper from '../shared/Wrapper'
 import styled from 'styled-components'
 import {BasicTextStyle, BoldTextStyle, MediumTextStyle, Color} from '../shared/style'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useForm } from 'react-hook-form'
+import {navigate} from 'gatsby'
+
+const encode = (data) => {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+};
 
 function Form() {
+  const { register, handleSubmit, watch, errors } = useForm();
+  const onSubmit = (data) => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({"form-name": "contact",...data})
+    })
+      .then(() => navigate("/thanks/"))
+      .catch(() => alert("お問い合わせに失敗しました"));
+  };
   const data = useStaticQuery(graphql`
     query {
       site {
@@ -23,19 +41,20 @@ function Form() {
       </Title>
       <FormBlock
         name="contact"
-        method="POST"
         data-netlify="true"
         data-netlify-honeypot="bot-field"
-        action="/thanks"
+        onSubmit={handleSubmit(onSubmit)}
       >
-        <input type="hidden" name="form-name" value="contact" />
+        <input type="hidden" name="form-name" />
         <input type="hidden" name="bot-field" />
         <InputItem>
           <Label>
             お名前<Required>必須</Required>
             <Input
-              type='text' name='name' id='name' required
+              name='name'
+              ref={register({ required: 'お名前を入力してください'})}
             />
+            {errors.name && (<Error>{errors.name.message}</Error>)}
             <Ex>例）巴屋　花子</Ex>
           </Label>
         </InputItem>
@@ -43,9 +62,16 @@ function Form() {
           <Label>
             フリガナ<Required>必須</Required>
             <Input
-              type='text' name='furigana' id='furigana' required
-              pattern='^[ァ-ヶー　]+$'
+              name='furigana'
+              ref={register({
+                required: 'お名前(フリガナ)を入力してください',
+                pattern: {
+                  value: /^[ァ-ヶー　]+$/,
+                  message: '全角カタカナで入力してください'
+                }
+              })}
             />
+            {errors.furigana && (<Error>{errors.furigana.message}</Error>)}
             <Ex>例）トモエヤ　ハナコ</Ex>
           </Label>
         </InputItem>
@@ -53,9 +79,16 @@ function Form() {
           <Label>
             メールアドレス<Required>必須</Required>
             <Input
-              type='email' name='email' id='email' required
-              pattern='[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$'
+              name='email'
+              ref={register({
+                required: 'メールアドレスを入力してください',
+                pattern: {
+                  value: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/,
+                  message: 'メールアドレスを正しく入力してください'
+                }
+              })}
             />
+            {errors.email && (<Error>{errors.email.message}</Error>)}
             <Ex>例）tomoeya-hanako@abc.com</Ex>
           </Label>
         </InputItem>
@@ -63,16 +96,23 @@ function Form() {
           <Label>
             電話番号<Required>必須</Required>
             <Input
-              type='tel' name='tel' id='tel' required
-              pattern='\d{2,4}-\d{3,4}-\d{3,4}'
+              name='tel'
+              ref={register({
+                required: '電話番号を入力してください',
+                pattern: {
+                  value: /\d{2,4}-\d{3,4}-\d{3,4}/,
+                  message: 'ハイフンを入れて半角数字で入力してください'
+                }
+              })}
             />
+            {errors.tel && (<Error>{errors.tel.message}</Error>)}
             <Ex>例）052-123-4567</Ex>
           </Label>
         </InputItem>
         <InputItem>
           <Label>
             会社名
-            <Input type='text' name='company-name' id='company-name'/>
+            <Input name='company-name' ref={register}/>
             <Ex>例）有限会社トモエ屋</Ex>
           </Label>
         </InputItem>
@@ -80,30 +120,45 @@ function Form() {
           <Label>
             郵便番号
             <Input
-              type='text' name='postal-code' id='postal-code'
-              pattern='\d{3}-\d{4}'
+              name='postalCode'
+              ref={register({
+                pattern: {
+                  value: /\d{3}-\d{4}/,
+                  message: '郵便番号を正しく入力してください'
+                }
+              })}
             />
+            {errors.postalCode && (<Error>{errors.postalCode.message}</Error>)}
             <Ex>例）000-0000</Ex>
           </Label>
         </InputItem>
         <InputItem>
           <Label>
             住所
-            <Input type='text' name='address' id='address'/>
+            <Input name='address' ref={register}/>
             <Ex>例）愛知県名古屋市名古屋町00-00</Ex>
           </Label>
         </InputItem>
         <InputItem>
           <Label>
             建物名
-            <Input type='text' name='building-name' id='building-name'/>
+            <Input name='building-name' ref={register}/>
             <Ex>例）トモエヤビル0階</Ex>
           </Label>
         </InputItem>
         <InputItem>
           <Label>
             問い合わせ内容<Required>必須</Required>
-            <Textarea name='message' id='message' required/>
+            <Textarea name='message'
+            ref={register({
+              required: 'お問い合わせ内容を入力してください',
+              maxLength : {
+                value: 300,
+                message: '300文字以内で入力してください'
+              }
+            })}
+            />
+            {errors.message && (<Error>{errors.message.message}</Error>)}
           </Label>
         </InputItem>
         <InputItem>
@@ -120,8 +175,12 @@ function Form() {
         </InputItem>
         <InputItem className='checkbox'>
           <Label>
-            <input type="checkbox"/>
+            <input
+              type="checkbox" name="privacy"
+              ref={register({ required: "チェックを入れてください" })}
+            />
             個人情報の取扱いについて同意します。
+            {errors.privacy && (<Error>{errors.privacy.message}</Error>)}
           </Label>
         </InputItem>
         <Button type='submit'>送信する</Button>
@@ -183,6 +242,11 @@ const Input = styled.input`
 const Ex = styled.p`
   ${MediumTextStyle}
   color: #777;
+`
+
+const Error = styled.p`
+  ${MediumTextStyle}
+  color: #ff9999;
 `
 
 const Textarea = styled.textarea`
