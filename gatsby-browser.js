5dabbf6bdd922fd753aaec9dddfb1edb41c15720
Ayano Taki <ayanobayern2@gmail.com>
2021-03-19 21:27:50 +0900

    fontawsome修正

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,6 +5,10 @@
  */
 
 // You can delete this file if you're not using it
+const styles = require("@fortawesome/fontawesome-svg-core/styles.css")
+const config = require("@fortawesome/fontawesome-svg-core")
+
+config.autoAddCss = false
 exports.shouldUpdateScroll = () => {
   if (window.__navigatingToLink === true) {
     return [0, 0];
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,6 +6,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
 import { faPhoneAlt, faFax, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { config } from '@fortawesome/fontawesome-svg-core'
 import Header from './Header/Header'
 import Footer from './Footer/Footer'
 
