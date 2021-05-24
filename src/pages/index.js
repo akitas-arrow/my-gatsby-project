eb31b61f1a94fca94b38bfdc10959c59c7239c16
Ayano Taki <ayanobayern2@gmail.com>
2021-05-24 16:44:56 +0900

    instagram投稿表示

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,9 @@ import React from "react"
 import Layout from "../components/Layout"
 import News from "../components/Home/News"
 import Service from "../components/Home/Service"
-import About from "../components/Home/About"
 import LineUp from "../components/Home/LineUp"
+import About from "../components/Home/About"
+import Instagram from "../components/Home/Instagram"
 import ContactButton from "../components/shared/ContactButton"
 import StyledMainImage from "../components/Home/MainImage"
 import SEO from '../components/shared/seo'
@@ -19,6 +20,7 @@ const IndexPage = () => {
       <Service />
       <LineUp/>
       <About />
+      <Instagram />
     </Layout>
   )
 }
